32ae7c8514c538919588ab3f696801493a46e73b
Jonathan D. Baker <jonathan@ardent.tech>
2017-09-30 17:41:25 -0600

    testing and model utils

---
+++ README.md
@@ -0,0 +1,12 @@
+# Django API Skeleton
+
+An Python3.x RESTful API skeleton based on [Django Rest Framework](http://www.django-rest-framework.org).
+
+## Setup
+
+1. `$ cd ~/ && mkdir .venv && cd .venv`
+2. `$ python3.5 -m venv <project_name>`
+3. `$ source ./django_api_skeleton/bin/activate`
+4. `$ cd ~/ && mkdir -p projects/<project_name> && cd projects/<project_name>`
+5. `$ git clone git@github.com:ardentTech/django_api_skeleton.git .`
+6. `$ pip install -r requirements.txt`
---
+++ api/util/testing.py
@@ -0,0 +1,24 @@
+import json
+
+from rest_framework.test import APITestCase
+
+
+class BaseAPITestCase(APITestCase):
+
+    def assert_bad_request(self, response):
+        self.assertEqual(response.status_code, 400)
+
+    def assert_get_ok(self, response, **kwargs):
+        count = kwargs.get("count", 0)
+        self.assertEqual(response.status_code, 200)
+        if count > 0:
+            self.assertEqual(self.get_content(response)["count"], count)
+
+    def assert_not_found(self, response):
+        self.assertEqual(response.status_code, 404)
+
+    def assert_post_ok(self, response):
+        self.assertEqual(response.status_code, 201)
+
+    def get_content(self, response):
+        return json.loads(response.content.decode("utf-8"))
